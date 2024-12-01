e3102a34aa076ea7e6f3aa35504f1372ce9ad29f
Leonardo Contreras <joseleoc123@gmail.com>
2024-12-01 00:55:39 -0400

    SCRUM-5 feat(auth): add gradient background and improve layout for welcome screen, enhancing visual design and user experience

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,11 @@
 import { Image } from "expo-image";
-import { View } from "react-native";
 import { router } from "expo-router";
 import { Text } from "react-native-paper";
 import { StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import { useTheme } from "react-native-paper";
+import { View, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { FontStyles } from "@/constants/Styles";
@@ -16,70 +17,78 @@ export default function Welcome() {
   };
   return (
     <SafeAreaView>
-      <View style={[styles.section, { backgroundColor: colors.primary }]}>
-        <View style={styles.branding_container}>
-          <Image
-            source={require("../../assets/images/icon.png")}
-            style={styles.logo}
-          />
-          <View style={styles.branding_text_container}>
-            {["Healthy", "Food"].map((str, ind) => (
+      <ImageBackground
+        source={require("../../assets/images/backgrounds/bell-bg.jpg")}
+      >
+        <LinearGradient
+          colors={["transparent", "rgba(0, 0, 0, 0.7)"]}
+          style={styles.gradient_mask}
+        />
+        <View style={[styles.section]}>
+          <View style={styles.branding_container}>
+            <Image
+              source={require("../../assets/images/icon.png")}
+              style={styles.logo}
+            />
+            <View style={styles.branding_text_container}>
+              {["Healthy", "Food"].map((str, ind) => (
+                <Text
+                  key={ind}
+                  variant="headlineMedium"
+                  style={[{ color: colors.onPrimary }, FontStyles.headline]}
+                >
+                  {str}
+                </Text>
+              ))}
+            </View>
+          </View>
+
+          <View style={styles.title_container}>
+            {["Tu", "app", "saludable"].map((str, ind) => (
               <Text
                 key={ind}
-                variant="headlineMedium"
-                style={[{ color: colors.onPrimary }, FontStyles.headline]}
+                variant="displayLarge"
+                style={[
+                  { color: colors.onPrimary },
+                  FontStyles.headline,
+                  styles.title,
+                ]}
               >
                 {str}
               </Text>
             ))}
           </View>
-        </View>
 
-        <View style={styles.title_container}>
-          {["Tu", "app", "saludable"].map((str, ind) => (
-            <Text
-              key={ind}
-              variant="displayLarge"
-              style={[
-                { color: colors.onPrimary },
-                FontStyles.headline,
-                styles.title,
+          <View style={styles.actions_container}>
+            <Button
+              onPress={() => handleNavigate("sign-in")}
+              mode="contained"
+              textColor={colors.onSecondary}
+              buttonColor={colors.secondary}
+              contentStyle={[
+                styles.button,
+                { backgroundColor: colors.secondary },
               ]}
+              labelStyle={[styles.button_text]}
             >
-              {str}
-            </Text>
-          ))}
+              Iniciar sesión
+            </Button>
+            <Button
+              onPress={() => handleNavigate("sign-up")}
+              mode="contained"
+              textColor={colors.onBackground}
+              buttonColor={colors.background}
+              contentStyle={[
+                styles.button,
+                { backgroundColor: colors.background },
+              ]}
+              labelStyle={[styles.button_text]}
+            >
+              Registrarme
+            </Button>
+          </View>
         </View>
-
-        <View style={styles.actions_container}>
-          <Button
-            onPress={() => handleNavigate("sign-in")}
-            mode="contained"
-            textColor={colors.onSecondary}
-            buttonColor={colors.secondary}
-            contentStyle={[
-              styles.button,
-              { backgroundColor: colors.secondary },
-            ]}
-            labelStyle={[styles.button_text]}
-          >
-            Iniciar sesión
-          </Button>
-          <Button
-            onPress={() => handleNavigate("sign-up")}
-            mode="contained"
-            textColor={colors.onBackground}
-            buttonColor={colors.background}
-            contentStyle={[
-              styles.button,
-              { backgroundColor: colors.background },
-            ]}
-            labelStyle={[styles.button_text]}
-          >
-            Registrarme
-          </Button>
-        </View>
-      </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
@@ -91,24 +100,31 @@ const styles = StyleSheet.create({
     paddingHorizontal: 64,
     alignContent: "space-between",
     justifyContent: "center",
-    gap: 48,
+    gap: 16,
+  },
+  gradient_mask: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
   },
   branding_container: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 4,
+    gap: 16,
   },
   logo: {
-    width: 100,
-    height: 100,
-    borderRadius: 1,
+    width: 75,
+    height: 75,
+    borderRadius: 15,
   },
   branding_text_container: {
     flexDirection: "column",
     alignItems: "flex-start",
   },
   title_container: {
-    gap: 8,
+    gap: 14,
   },
   title: {
     fontWeight: "bold",
@@ -116,6 +132,7 @@ const styles = StyleSheet.create({
   actions_container: {
     gap: 16,
     width: "100%",
+    marginTop: 92,
   },
   button_text: {
     fontWeight: "bold",
